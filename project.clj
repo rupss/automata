e7f6cdfc3a65c514d5475d05ab979f31f105bba1
Rupa Shankar <rupsshankar@gmail.com>
2013-11-20 16:50:14 -0500

    Implemented the DFA that accepts multiples of 3 using core.logic (logical approach

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.logic "0.8.4"]]
   :main automata.core
   :profiles {:uberjar {:aot :all}})
--- src/automata/core.clj
+++ src/automata/core.clj
@@ -1,5 +1,7 @@
 (ns automata.core
-  (:gen-class))
+  (:gen-class)
+  (:use [clojure.core.logic])
+  (:require [clojure.core.logic.fd :as fd]))
 
 (defn mult-3*
   [input curr-state]
@@ -22,7 +24,31 @@
   [input]
   (mult-3* (seq (Integer/toString input)) :S0))
 
+(defn mult-3-logico*
+  [input curr-state]
+  (run* [q]
+        (conde
+         [(== curr-state :S0)
+          (conde
+           [(== '() input) (== q 'accept)]
+           [(== (first input) \0) (== q (mult-3-logico* (rest input) :S0))]
+           [(== (first input) \1) (== q (mult-3-logico* (rest input) :S1))])]
+         [(== curr-state :S1)
+           (conde
+           [(== '() input) (== q 'reject)]
+           [(== (first input) \0) (== q (mult-3-logico* (rest input) :S2))]
+           [(== (first input) \1) (== q (mult-3-logico* (rest input) :S0))])]
+         [(== curr-state :S2)
+          (conde
+           [(== '() input) (== q 'reject)]
+           [(== (first input) \0) (== q (mult-3-logico* (rest input) :S1))]
+           [(== (first input) \1) (== q (mult-3-logico* (rest input) :S2))])])))
+
+(defn mult-3-logico
+  [input]
+  (mult-3-logico* (seq (Integer/toString input)) :S0))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (mult-3 1001)))
