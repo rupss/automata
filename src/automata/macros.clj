4d8bd5fe217735fad0be9483a4df19691e7ca98f
Rupa Shankar <rupsshankar@gmail.com>
2013-11-25 16:35:42 -0500

    There's some gensym issue

--- src/automata/macros.clj
+++ src/automata/macros.clj
@@ -34,25 +34,100 @@
   [x]
   `(test1 ~x))
 
-(def test-dfa {:S0 {:start true :result :accept :transitions {0 :S0 1 :S1}}
-               :S1 {:result :reject :transitions {0 :S0 1 :S1}}})
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def test-dfa {'S0 {:start true :result :accept :transitions {0 'S0 1 'S1}}
+               'S1 {:result :reject :transitions {0 'S0 1 'S1}}})
 
 (defn build-transition
   [[char state]]
-  `([[~char . ?rem] ?out] (~state ?rem ?out)))
+  `([[~char . ?rem#] ?out] (~state ?rem# ?out#)))
 
 (defn my-matche
   [state]
   (let [trans-vecs (seq (:transitions state))]
-    `(matche [input out]
-             ([[] ~(:result state)])
-            ; ~(build-transition (first trans-vecs))
-             ~@(map build-transition trans-vecs))))
+    (cons `([[] ~(:result state)])
+          `(~@(map build-transition trans-vecs)))))
 
 (defn write-state-function
-  [state-name state]
-  `(~state-name [input output] ~(my-matche state)))
- 
+  [[state-name state]]
+  `(~state-name [input# out#]
+                (matche [input# out#]
+                        ~@(my-matche state))))
+
+(defn get-start-state
+  [dfa]
+  (-> (filter (fn [[name state]] (-> state :start nil? not)) (seq dfa)) first first))
+
+(defn write-run-expr
+  [dfa]
+  `(defn my-dfa [input#]
+     (run 5 [q#]
+           (letfn [~@(map write-state-function (seq dfa))]
+             (~(get-start-state dfa) input# q#)))))
+
+(defmacro build-automata-fn [dfa]
+  `~(write-run-expr dfa))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn get-vec
+  []
+  [1 2 3])
+
+(defn hello
+  []
+  `(defn foo [] (println "hello world")))
+
+(defmacro func []
+  `~(hello))
+
+
+;works
+(defmacro test
+  [dfa]
+  `(defn my-dfa [input#]
+     ~(p)))
+
+
+;; works
+(defmacro test
+  [dfa]
+  `(defn my-dfa [input#]
+     (run 10 [q#] (== q# input#))))
+
+;; works
+(defmacro test
+  []
+  `(run 10 [q#]
+       (matche [[\a \b \c] q#]
+               ([[] :accept])
+               ([[\a . ?rem#] :reject]))))
+;; works
+(defmacro test
+  []
+  `(defn basic [input#]
+     (run 10 [q#]
+          (conde
+           [
+            (matche [input# q#]
+                    ([[] :accept])
+                    ([[\a . ?rem#] :reject]))]
+           [(== q# input#)]))))
+
+(run 10 [q])
+
+;; works
+(defmacro test
+  [val]
+  `(defn pval [input#]
+     (println input#)))
+
+;works
+(defmacro foo
+  []
+  `(defn bar [x#] (println "hello")))
+  
 (defn mult-3-logico
   [input]
   (run 10 [q] 
@@ -75,3 +150,37 @@
                          ([[\0 . ?rem] ?out] (S1 ?rem ?out))
                          ([[\1 . ?rem] ?out] (S2 ?rem ?out))))]
          (S0 (seq (Integer/toString input)) q))))
+
+
+
+(def
+ automata.core/my-dfa
+ (clojure.core/fn
+  ([input__6975__auto__]
+   (clojure.core.logic/run
+    5
+    [q__6972__auto__]
+    (clojure.core/letfn
+        [
+
+      (S0
+       [input__7150__auto__ out__7151__auto__]
+       (clojure.core.logic/matche
+        [input__7023__auto__ out__7024__auto__]
+        ([[] :accept])
+        ([[0 . ?rem__6997__auto__] ?out__6998__auto__]
+         (S0 ?rem__6997__auto__ ?out__6998__auto__))
+        ([[1 . ?rem__6997__auto__] ?out__6998__auto__]
+          (S1 ?rem__6997__auto__ ?out__6998__auto__))))
+
+      
+      (S1
+       [input__7150__auto__ out__7151__auto__]
+       (clojure.core.logic/matche
+        [input__7023__auto__ out__7024__auto__]
+        ([[] :reject])
+        ([[0 . ?rem__6997__auto__] ?out__6998__auto__]
+         (S0 ?rem__6997__auto__ ?out__6998__auto__))
+        ([[1 . ?rem__6997__auto__] ?out__6998__auto__]
+         (S1 ?rem__6997__auto__ ?out__6998__auto__))))]
+     (S0 input__6973__auto__ q__6972__auto__))))))
